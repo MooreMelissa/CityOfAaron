b4bf460bb903ef7d34dee340e16f2ef4306ba308
Heather Holt <hol16046@byui.edu>
2018-05-29 13:14:59 -0600

    Outline of Code for L06 - Team Assignment
    Outlined the code necessary for our function (with a lot of comment questions). I'm not sure how to implement some things our code needs or how to test it.

--- CityOfAaron/src/control/GameControl.java
+++ CityOfAaron/src/control/GameControl.java
@@ -5,6 +5,8 @@
  */
 package control;
 
+import model.Game;
+
 /**
  *
  * @author heatherholt
@@ -22,25 +24,30 @@ public class GameControl{
 	 * setAcresOwned = getAcresOwned + acresToBuy 
 	 * setWheatInStorage = getWheatInStorage – cost
 	 * END 
+	 * @param acresToBuy
+	 * @param randomPrice
+	 * @param game
+	 * @return 
 	 */
 	
-	public void buyLand(int acresToBuy, int randomPrice, int getAcresOwned, int getWheatInStorage) {
+	public static int buyLand(int acresToBuy, int randomPrice, Game game) { /** Per Bro. Anderson's reply to my DB post about static, the method has been declared as static and I have added the game class as a parameter which should let us grab anything we need from the game. */
 		
 		randomPrice = 0; /** random number between 17 and 27 **How to implement the random number? */
 		
 		if (acresToBuy < 0) {
-			return; /** *per page 113 in the textbook this should end the function (can't return -1 without an error) **How to indicate an error? */
+			return -1; /** *per page 113 in the textbook return; should end the function (can't return -1 without the function returning an int. To eliminate some errors at this point, I have the function returning an int.) **How to indicate an error? */
 		}
 		
 		int cost = (acresToBuy * randomPrice);
 		
-		if (cost > getWheatInStorage) {
-			return; /** *per page 113 in the textbook this should end the function (can't return -1 without an error) **How to indicate an error? */
+		if (cost > game.getWheatInStorage()) {
+			return -1; /** *per page 113 in the textbook return; should end the function (can't return -1 without the function returning an int. To eliminate some errors at this point, I have the function returning an int.) **How to indicate an error? */
 		}
 		
-		int setAcresOwned = getAcresOwned + acresToBuy; /** *How to implement getters/setters at this phase? Set as int for now  */
-		int setWheatInStorage = getWheatInStorage - cost; /** *How to test this? Java doesn't return multiple values, we won't know if the function returns the right numbers in the testing without setting a return here.   */
-		
+		int setAcresOwned = game.getAcresOwned() + acresToBuy; /** *How to implement getters/setters at this phase? Set as int for now  */
+		int setWheatInStorage = game.getWheatInStorage() - cost; /** *How to test this? Java doesn't return multiple values, we won't know if the function returns the right numbers in the testing without setting a return here.   */
+				
+		return setAcresOwned; /** returning an int for the sake of testing */
 	}
 	
 	
