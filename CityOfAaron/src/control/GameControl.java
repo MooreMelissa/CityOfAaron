e3ae285e915714909632db811babbc08d778ab0e
melissamoore <melissamoore@192.168.1.4>
2018-05-30 21:18:15 -0500

    random numbers live the year

--- CityOfAaron/src/control/GameControl.java
+++ CityOfAaron/src/control/GameControl.java
@@ -6,30 +6,43 @@
 package control;
 
 import model.Game;
-
+import java.util.Random;
 /**
  *
  * @author heatherholt
  */
 public class GameControl{
 	
-	static Game game = new Game();
 	
 	int acresToBuy = 80; //get user input
-	int randomPrice = 22; //get random number between 17 and 27		
+	
+        int acresToPlant = 0; // get user input
 	
 	
-	public void liveTheYear() {
-		//harvestWheat() - H
+	public boolean liveTheYear(Game game, int acresToBuy, int acresToPlant) {
+	    int totalWheat = game.getWheatInStorage();
+            int randomPrice = RandomNumbers.getRandom(17, 27);
+                 //harvestWheat() - H
 		//wheatOfferings() - R
 		//wheatEatenRats() - H
 		//populationMortality() - M
 		//peopleMoveIn() - R
 		
-		buyLand (acresToBuy, randomPrice, game.getWheatInStorage(), game.getAcresOwned());
-		//sellLand() - H
+		int landCostInWheat = buyLand (acresToBuy, randomPrice, game.getWheatInStorage(), game.getAcresOwned());
+		if (landCostInWheat < 0) {
+                    return false;
+                }
+                totalWheat = totalWheat - landCostInWheat;
+                //sellLand() - H
 		//feedPeople() - M
-		//plantCrops() - M
+		
+                plantCrops(acresToPlant,
+                           game.getAcresOwned(), 
+                           game.getCurrentPopulation(), 
+        
+                          game.getWheatInStorage()); 
+                
+                return true;
 	}
 	
 	/**
@@ -68,11 +81,49 @@ public class GameControl{
 		
 	}
 	
+	/**
+         * public void plantCrops(int acresToPlant, int getAcresOwned, int getWheatInStorage, int getCurrent Population)
+         *BEGIN
+         *IF (acresToPlant  <  0)  THEN  RETURN -1
+         *IF (acresToPlant  >  getAcresOwned)  THEN RETURN -2
+         *IF (acresToPlant  >  getCurrentPopulation * 10)  THEN RETURN -3
+         *IF (acresToPlant / 2 > getWheatInStorage) THEN RETURN -4
+         *setWheatInStorage = getWheatInStorage  - (acresToPlant / 2)
+         *setAcresPlanted = acresToPlant
+         *END
+         * @param acresToPlant
+         * @param acresOwned
+         * @param wheatInStorage
+         * @param currentPopulation
+         * @return
+        */
+
+        public static int plantCrops(int acresToPlant, int acresOwned, int currentPopulation, int wheatInStorage) {
+
+            if (acresToPlant < 0) {
+                return -1;
+            }
+            
+            if (acresToPlant > acresOwned) {
+                return -2;
+            }
+            
+            if (acresToPlant > currentPopulation * 10) {
+                return -3;
+            }
+            
+            if (acresToPlant / 2 > wheatInStorage) {
+                return -4;
+            }
 	
+            game.setWheatInStorage(wheatInStorage - (acresToPlant / 2));
+            
+            game.setAcresPlanted(acresToPlant);
+            
+            //return acresToPlant;
+            return wheatInStorage - (acresToPlant/ 2);
 	
-	
-	
-	
+        }
 	
 	
 	
