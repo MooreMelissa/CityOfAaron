7a7b030f1e893b17d8a8eec76b9e0a9e6e2ec374
JK <JK@192.168.1.7>
2018-06-27 08:43:46 -0500

    Individual Assignment - Tools

--- CityOfAaron/src/control/StorehouseControl.java
+++ CityOfAaron/src/control/StorehouseControl.java
@@ -12,32 +12,46 @@ import model.InventoryItem;
  * @author heatherholt
  */
 public class StorehouseControl {
-	public static InventoryItem[] sortAnimals(InventoryItem[] animals) {
-		for (int a=0; a < animals.length-1; a++) {
-			for (int b=a+1; b < animals.length; b++) {
-				if (animals[a].getName().compareTo(animals[b].getName()) > 0) {
-					InventoryItem temp = animals[a];
-					animals[a] = animals[b];
-					animals[b] = temp;
-				}
-			}	
-		}
-		return animals;
-	}
-	
-	public static InventoryItem[] sortProvisions(InventoryItem[] provisions) {
-		for (int i=0; i < provisions.length-1; i++) {
-                    
-			for(int j = i + 1; j < provisions.length; j++) {
 
-				if (provisions[i].getCondition().compareTo(provisions[j].getCondition()) > 0) {
-					InventoryItem temp = provisions[i];
-					provisions[i] = provisions[j];
-					provisions[j] = temp;
+    public static InventoryItem[] sortAnimals(InventoryItem[] animals) {
+        for (int a = 0; a < animals.length - 1; a++) {
+            for (int b = a + 1; b < animals.length; b++) {
+                if (animals[a].getName().compareTo(animals[b].getName()) > 0) {
+                    InventoryItem temp = animals[a];
+                    animals[a] = animals[b];
+                    animals[b] = temp;
+                }
+            }
+        }
+        return animals;
+    }
 
-				} 
-			}
-		}
-		return provisions;
-	}
+    public static InventoryItem[] sortTools(InventoryItem[] tools) {
+        for (int i = 0; i < tools.length - 1; i++) {
+            for (int j = i + 1; j < tools.length; j++) {
+                if (tools[i].getName().compareTo(tools[j].getName()) > 0) {
+                    InventoryItem temp = tools[i];
+                    tools[i] = tools[j];
+                    tools[j] = temp;
+                }
+            }
+        }
+        return tools;
+    }
+
+    public static InventoryItem[] sortProvisions(InventoryItem[] provisions) {
+        for (int i = 0; i < provisions.length - 1; i++) {
+
+            for (int j = i + 1; j < provisions.length; j++) {
+
+                if (provisions[i].getCondition().compareTo(provisions[j].getCondition()) > 0) {
+                    InventoryItem temp = provisions[i];
+                    provisions[i] = provisions[j];
+                    provisions[j] = temp;
+
+                }
+            }
+        }
+        return provisions;
+    }
 }
--- CityOfAaron/src/view/ReportsMenuView.java
+++ CityOfAaron/src/view/ReportsMenuView.java
@@ -19,109 +19,110 @@ import model.Storehouse;
  */
 public class ReportsMenuView extends ViewBase {
 
-	Game game = CityOfAaron.getCurrentGame();
-	Storehouse storehouse = game.getTheStorehouse();
+    Game game = CityOfAaron.getCurrentGame();
+    Storehouse storehouse = game.getTheStorehouse();
 
-	/**
-	 * Constructor
-	 */
-	public ReportsMenuView() {
+    /**
+     * Constructor
+     */
+    public ReportsMenuView() {
 
-	}
+    }
 
-	@Override
-	protected String getMessage() {
-		return "\n\nReports Menu\n"
-				+ "-------------\n"
-				+ "A - View Animals in Storehouse\n"
-				+ "T - View Tools in Storehouse\n"
-				+ "P - View Provisions in Storehouse\n"
-				+ "V - View Authors of Game\n"
-				+ "Q - Return to Game Menu";
-	}
+    @Override
+    protected String getMessage() {
+        return "\n\nReports Menu\n"
+                + "-------------\n"
+                + "A - View Animals in Storehouse\n"
+                + "T - View Tools in Storehouse\n"
+                + "P - View Provisions in Storehouse\n"
+                + "V - View Authors of Game\n"
+                + "Q - Return to Game Menu";
+    }
 
-	/**
-	 * Get the set of inputs from the user.
-	 * @return
-	 */
-	@Override
-	public String[] getInputs() {
+    /**
+     * Get the set of inputs from the user.
+     *
+     * @return
+     */
+    @Override
+    public String[] getInputs() {
 
-		// Declare the array to have the number of elements you intend to get 
-		// from the user.
-		String[] inputs = new String[1];
+        // Declare the array to have the number of elements you intend to get 
+        // from the user.
+        String[] inputs = new String[1];
 
-		inputs[0] = getUserInput("Please choose a menu option:");
+        inputs[0] = getUserInput("Please choose a menu option:");
 
-		// Repeat for each input you need, putting it into its proper slot in the array.
-		return inputs;
-	}
+        // Repeat for each input you need, putting it into its proper slot in the array.
+        return inputs;
+    }
 
-	/**
-	 * Perform the action indicated by the user's input.
-	 * @param inputs
-	 * @return true if the view should repeat itself, and false if the view
-	 * should exit and return to the previous view.
-	 */
-	@Override
-	public boolean doAction(String[] inputs) {
+    /**
+     * Perform the action indicated by the user's input.
+     *
+     * @param inputs
+     * @return true if the view should repeat itself, and false if the view
+     * should exit and return to the previous view.
+     */
+    @Override
+    public boolean doAction(String[] inputs) {
 
-		switch (inputs[0].trim().toUpperCase()) {
-			case "A":
-				viewAnimals();
-				break;
-			case "T":
-				viewTools();
-				break;
-			case "P":
-				viewProvisions();
-				break;
-			case "V":
-				viewAuthors();
-				break;
-			case "Q":
-				return false;
-		}
+        switch (inputs[0].trim().toUpperCase()) {
+            case "A":
+                viewAnimals();
+                break;
+            case "T":
+                viewTools();
+                break;
+            case "P":
+                viewProvisions();
+                break;
+            case "V":
+                viewAuthors();
+                break;
+            case "Q":
+                return false;
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	// Define your action handlers here. These are the methods that your doAction()
-	// method will call based on the user's input. We don't want to do a lot of 
-	// complex game stuff in our doAction() method. It will get messy very quickly.
-	private void viewAnimals() {
-		InventoryItem[] animals = StorehouseControl.sortAnimals(storehouse.getAnimals());	
-		for (InventoryItem animal : animals) {
-			System.out.println(animal);
-		}
-		pause(2000);
-	}
+    // Define your action handlers here. These are the methods that your doAction()
+    // method will call based on the user's input. We don't want to do a lot of 
+    // complex game stuff in our doAction() method. It will get messy very quickly.
+    private void viewAnimals() {
+        InventoryItem[] animals = StorehouseControl.sortAnimals(storehouse.getAnimals());
+        for (InventoryItem animal : animals) {
+            System.out.println(animal);
+        }
+        pause(2000);
+    }
 
-	private void viewTools() {
-		InventoryItem[] tools = storehouse.getTools();
-		Arrays.sort(tools, (a,b) -> a.getName().compareTo(b.getName()));
-            for (InventoryItem tool : tools) {
-                System.out.print(tool);
-            }
-		pause(2000);
-	}
+    private void viewTools() {
+        InventoryItem[] tools = StorehouseControl.sortTools(storehouse.getTools());
+        for (InventoryItem tool : tools) {
+            System.out.print(tool);
+        }
+        pause(2000);
+    }
 
-	private void viewProvisions() {
-		InventoryItem[] provisions = StorehouseControl.sortProvisions(storehouse.getProvisions());
-		for(InventoryItem provision: provisions){
-			System.out.print(provision);
-		}
-		pause(2000);
-	} 
+    private void viewProvisions() {
+        InventoryItem[] provisions = StorehouseControl.sortProvisions(storehouse.getProvisions());
+        for (InventoryItem provision : provisions) {
+            System.out.print(provision);
+        }
+        pause(2000);
+    }
 
-	private void viewAuthors() {
-		//System.out.println("* viewAuthors() called. Implementation coming soon. *");
-		Author[] authors = storehouse.getAuthors();
-		int arrayLength = authors.length;
-		for (int i = 0; i < arrayLength; i++) {
-			System.out.print(authors[i]);
-		}
-		pause(2000);
-	}
+    private void viewAuthors() {
+        //System.out.println("* viewAuthors() called. Implementation coming soon. *");
+        Author[] authors = storehouse.getAuthors();
+        int arrayLength = authors.length;
+        for (int i = 0; i < arrayLength; i++) {
+            System.out.print(authors[i]);
+        }
+        pause(2000);
+    }
 
 }
